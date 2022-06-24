a27517abe7086af66dc71d55f700d97a1d02d37c
c4ffein <c4ffein>
2022-06-24 13:04:36 +0200

    no interface

--- imapfw/architects/architect.py
+++ imapfw/architects/architect.py
@@ -3,42 +3,22 @@
 
 """
 
-The achitects are high level objects to support actions with dynamic process
-handling.
+The achitects are high level objects to support actions with dynamic process handling.
 
-They are helpers for the actions/softwares. They handles workers and whatever
-required to enable other components.
+They are helpers for the actions/softwares. They handles workers and whatever required to enable other components.
 
-This is the wrong place for anything about business logic. Architects are not
-problem solving for the purpose of the actions/softwares.
+This is the wrong place for anything about business logic.
+Architects are not problem solving for the purpose of the actions/softwares.
 
 """
 
 from imapfw import runtime
 
-from .debug import debugArchitect
-
-# Interfaces.
-from imapfw.interface import Interface, implements
-
 # Annotations.
 from imapfw.annotation import Function
 
 
-class ArchitectInterface(Interface):
-
-    def kill(self) -> None:
-        """Kill worker."""
-
-    def start(self, runner: Function, runnerArgs: tuple) -> None:
-        """Start worker."""
-
-    def stop(self) -> None:
-        """Stop worker."""
-
-
 @debugArchitect
-@implements(ArchitectInterface)
 class Architect(object):
     def __init__(self, workerName: str):
         self.workerName = workerName
@@ -47,11 +27,14 @@ class Architect(object):
         self.worker = None
 
     def kill(self) -> None:
+        """Kill worker."""
         self.worker.kill()
 
     def start(self, runner: Function, runnerArgs: tuple) -> None:
+        """Start worker."""
         self.worker = runtime.concurrency.createWorker(self.workerName, runner, runnerArgs)
         self.worker.start()
 
     def stop(self) -> None:
+        """Stop worker."""
         self.worker.join()
--- imapfw/architects/engine.py
+++ imapfw/architects/engine.py
@@ -12,42 +12,17 @@ from .driver import DriversArchitect
 
 from .debug import debugArchitect
 
-# Interfaces.
-from imapfw.interface import Interface, implements, checkInterfaces
-
 # Annotations.
 from imapfw.edmp import Emitter
 from imapfw.annotation import Function
 
 
-class EngineArchitectInterface(Interface):
+@debugArchitect
+class EngineArchitect(object):
     """Architect for running an engine with both side drivers.
 
     Aggregate the engine architect and the drivers."""
 
-    def getLeftEmitter(self) -> Emitter:
-        """Return the emitter of the left-side driver."""
-
-    def getRightEmitter(self) -> Emitter:
-        """Return the emitter of the right-side driver."""
-
-    def init(self) -> None:
-        """Initialize the architect. Helps to compose components easily."""
-
-    def kill(self) -> None:
-        """Kill workers."""
-
-    def start(self, runner: Function, runnerArgs: tuple) -> None:
-        """Start the workers."""
-
-    def stop(self) -> None:
-        """Stop workers."""
-
-
-@debugArchitect
-@checkInterfaces()
-@implements(EngineArchitectInterface)
-class EngineArchitect(object):
     def __init__(self, workerName: str):
         self.workerName = workerName
 
@@ -55,24 +30,30 @@ class EngineArchitect(object):
         self.drivers = None
 
     def getLeftEmitter(self) -> Emitter:
+        """Return the emitter of the left-side driver."""
         return self.drivers.getEmitter(0)
 
     def getRightEmitter(self) -> Emitter:
+        """Return the emitter of the right-side driver."""
         return self.drivers.getEmitter(1)
 
     def init(self) -> None:
+        """Initialize the architect. Helps to compose components easily."""
         self.architect = Architect(self.workerName)
         self.drivers = DriversArchitect(self.workerName, 2)
         self.drivers.init()
 
     def kill(self) -> None:
+        """Kill workers."""
         self.drivers.kill()
         self.architect.kill()
 
     def start(self, runner: Function, runnerArgs: tuple) -> None:
+        """Start the workers."""
         self.drivers.start()
         self.architect.start(runner, runnerArgs)
 
     def stop(self) -> None:
+        """Stop workers."""
         self.drivers.stop()
         self.architect.stop()
